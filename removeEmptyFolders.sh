4f8068e33498d78203544aa5104f93cd3cfb12f7
rabaroman <rabaromandm@gmail.com>
2016-12-21 03:34:52 +0200

    Change removeEmptyFolders.sh

--- removeEmptyFolders.sh
+++ removeEmptyFolders.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 path=$1
-if [ -z $path ];
+if [ ! $path ];
     then echo "Enter path"
          exit 1
 else if cd $path;
-     then  find . -type d -empty -delete
-           echo "All folders are removed"
+     then     
+           emptyfolders="$(find . -type d -empty)"
+           if [[ $emptyfolders ]];
+           then
+              for var in $emptyfolders
+              do 
+                rmdir $var
+              done
+              echo "All folders are removed"
+           else
+               echo "Nothing to delete"
+               exit 1
+           fi
      else echo "Can't remove folders. Enter valid path"
           exit 1
-     fi
+    fi
 fi
